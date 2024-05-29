a24c64b1d485bdc9cf537b25dcd6cefaad8c239c
primer repositor <nadiabarolin@gmail.com>
2024-05-29 14:27:29 -0300

    Primera etapa

--- proyecyo_juego.py
+++ proyecyo_juego.py
@@ -5,45 +5,6 @@ def palabras_aleatoria():
     palabra_aleatoria = random.choice(palabras)
     return palabra_aleatoria
 
-def mostrar_guiones(palabra_secreta, letras_adivinadas):
-    tablero=" "
-    for letra in palabra_secreta:
-        if letra in letras_adivinadas:
-            tablero+=letra
-        else:
-            tablero+=" _ "
-    print(tablero)
-
-def juego():
-     palabra_secreta=palabras_aleatoria()
-     letras_adivinadas=[]
-     vidas = 5
-
-     while vidas>0:
-         mostrar_guiones(palabra_secreta, letras_adivinadas)
-         letra=input("Intruduce una letra: ").lower()  
-         
-         if letra in letras_adivinadas:
-             print("Ya adivinaste esa letra, ¡Intente otra!")
-             
-         
-             
-         if letra in palabra_secreta:
-           letras_adivinadas.append(letra)
-           if set(letras_adivinadas) == set(palabra_secreta):
-               print(f"La palabra es: {palabra_secreta.upper()}")
-               print(f"¡FELICITACIONES!! Has ganado!!")
-
-               break
-           
-         
-         else:
-             vidas-=1
-             print(f"Letra incorrecta. Te quedan: {vidas} vidas")
-
-         if vidas==0:
-          print(f"Has perdido. La palabra es: {palabra_secreta}")
-
 
 print(":::::::::::::::::::::: ADIVINA LA PALABRA :::::::::::::::::::::: ")
 
@@ -58,4 +19,3 @@ if respuesta.upper() == "SI":
 else:
     print("¡No hay problema! ¡Te esperamos en otro momento!")
 
-juego()
