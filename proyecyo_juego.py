91e1c414a020cab9230c0d62f9248949ca00f5a8
primer repositor <nadiabarolin@gmail.com>
2024-05-30 10:54:16 -0300

    Ultima etapa

--- proyecyo_juego.py
+++ proyecyo_juego.py
@@ -5,17 +5,63 @@ def palabras_aleatoria():
     palabra_aleatoria = random.choice(palabras)
     return palabra_aleatoria
 
+def mostrar_guiones(palabra_secreta, letras_adivinadas):
+    tablero=" "
+
+    for letra in palabra_secreta:
+        if letra in letras_adivinadas:
+            tablero+=letra
+        else:
+            tablero+="_ "
+            
+    print(f"\n\n{tablero}")         
+         
+
+def juego():
+     palabra_secreta=palabras_aleatoria()
+     letras_adivinadas=[]
+     vidas = 5
+     letras_usadas=[]
+
+     while vidas>0:
+         mostrar_guiones(palabra_secreta, letras_adivinadas)
+         letra=input("\nIntruduce una letra: ").lower()   
+            
+         
+         if letra in letras_adivinadas:
+             print("\nYa adivinaste esa letra, ¡Intente otra!")
+         else: 
+             letras_usadas.append(letra)
+             print(f"Letras usadas: {letras_usadas}")
+             
+         if letra in palabra_secreta:
+           letras_adivinadas.append(letra)
+           if set(letras_adivinadas) == set(palabra_secreta):
+               print(f"\nLa palabra es: {palabra_secreta.upper()}")
+               print(f"\n¡¡FELICITACIONES!! Has ganado!!")
+
+               break
+           
+         else:
+             vidas-=1
+             print(f"\nLetra incorrecta. Te quedan: {vidas} vidas")
+
+         if vidas==0:
+          print(f"\nHas perdido. La palabra es: {palabra_secreta.upper()}")
+          
 
 print(":::::::::::::::::::::: ADIVINA LA PALABRA :::::::::::::::::::::: ")
 
-name=input("Hola! Por favor ingrese su nombre: ")
+name=input("\nHola! Por favor ingrese su nombre: ")
 print(f"¡¡BIENVENIDO/A {name.upper()}!! \n\n ¿Estás listo/a para jugar?")
 
 respuesta=input("\n Respuesta SI o NO: ")
 if respuesta.upper() == "SI":
-    print("Tendras que escribir las letras para adivinar la palabra.")
+    print("\nTendras que escribir las letras para adivinar la palabra.")
     print("Tienes 5 vidas, si la letra es incorrecta, se descontaran vidas.")
     print("MUCHA SUERTE!!")
 else:
-    print("¡No hay problema! ¡Te esperamos en otro momento!")
-
+    print("\n¡No hay problema! ¡Te esperamos en otro momento!")
+    
+    
+juego()
